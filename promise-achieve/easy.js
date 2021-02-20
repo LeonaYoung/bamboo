aad0b71f55b5201c2323756d38dc89c932d077f2
Leona Yang <leona.y.young@gmail.com>
2021-02-20 20:36:50 +0800

    promise achieve

---
+++ promise-achieve/easy.js
@@ -0,0 +1,62 @@
+// 手写promise 简单版
+
+const PENDING = 'pending'
+const RESOLVED = 'resolved'
+const REJECTED = 'rejected'
+
+function MyPromise(fn) {
+  const that = this
+  that.state = PENDING
+  that.value = null
+  that.resolvedCallbacks = []
+  that.rejectedCallbacks = []
+
+  function resolve(value) {
+    if (that.state === PENDING) {
+      that.state = RESOLVED
+      that.value = value
+      that.resolvedCallbacks.map(cb => cb(that.value))
+    }
+  }
+
+  function reject(value) {
+    if (that.state === PENDING) {
+      that.state = REJECTED
+      that.value = value
+      that.rejectedCallbacks.map(cb => cb(that.value))
+    }
+  }
+
+  try {
+    fn(resolve, reject)
+  } catch(e) {
+    reject(e)
+  }
+}
+
+MyPromise.prototype.then = function(onFulfilled, onRejected) {
+  const that = this
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+  onRejected = typeof onRejected === 'function' ? onRejected : r => { throw r }
+
+  if (that.state === PENDING) {
+    that.resolvedCallbacks.push(onFulfilled)
+    that.rejectedCallbacks.push(onRejected)
+  }
+  if (that.state === RESOLVED) {
+    onFulfilled(that.value)
+  }
+  if (that.state === REJECTED) {
+    onRejected(that.value)
+  }
+}
+
+// 测试
+new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 0)
+  console.log('promise')
+}).then((value) => {
+  console.log(value)
+})
